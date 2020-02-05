0e71673f9bc2c6f90309526a132c737ca5a9a6cd
SaharCarmel <saharsela271@gmail.com>
2020-02-05 22:33:46 +0200

    Changed to new coach

--- rlpyt/agents/pg/coach.py
+++ rlpyt/agents/pg/coach.py
@@ -1,17 +1,21 @@
 import numpy as np
 from rlpyt.utils.logging import logger
+import numpy as np
+import math
 
 
 class Bandit():
     def __init__(self, env, beta, initialQ):
         self.env = env
         self.Q = initialQ
-        self.i = 0
+        self.counts = 1
         self.beta= beta
+        self.score = 0
 
-    def updateQ(self, reward, iteration):
-        self.Q = self.beta**(iteration - self.i)*self.Q + (1-self.beta**(iteration - self.i))*reward
-        self.i = iteration
+    def updateQ(self, reward, iteration, totalCounts):
+        self.Q = self.beta*self.Q + reward
+        self.counts = self.counts*self.beta + iteration
+        self.score = self.Q +math.sqrt((2*math.log(totalCounts))/(self.counts))
     
     def __repr__(self):
         return "Env:{} with current Q:{} and last time used in iteration:{}".format(self.env, self.Q, self.i)
@@ -22,10 +26,12 @@ class Coach():
         self.vectorSize = vectorSize
         self.envList = []
         self.algo = algo
+        self.beta = beta
         self.generateFunctions = {'Random': self.generateRandomVector,
                                   'Bandit': self.generateBanditVector}
         self.banditList = [Bandit(env, beta, initialQ) for env in envOptions]
         self.currentEnv = []
+        self.totalCounts = 0
 
     def generateVector(self, reward, iteration):
         reward = self._calculateAvgReturn(reward)
@@ -46,20 +52,25 @@ class Coach():
             self.envList.append(self.envOptions[int(_envNumber[0])])
         return self.envList
 
+    def sortQ(self, val):
+        return val.score
+
     def chooseEnv(self):
-        _maxEnv = ''
-        _tempMax = 0
-        for bandit in self.banditList:
-            if bandit.Q > _tempMax:
-                _maxEnv = bandit
-                _tempMax = bandit.Q
-                self.currentEnv = bandit
+        self.banditList.sort(key=self.sortQ, reverse=True)
+        self.currentEnv = self.banditList[0]
         for item in self.banditList:
             logger.record_tabular('Q-{}'.format(item.env), item.Q)
+            logger.record_tabular('Score-{}'.format(item.env), item.score)
+
         
 
     def generateBanditVector(self,reward, iteration):
-        self.currentEnv.updateQ(reward, iteration)
+        self.totalCounts = self.totalCounts*self.beta + 1
+        for bandit in self.banditList:
+            if bandit.env == self.currentEnv.env:
+                bandit.updateQ(reward, 1, totalCounts=self.totalCounts)
+            else:
+                bandit.updateQ(0,0,totalCounts=self.totalCounts)
         self.chooseEnv()
         self.envList = []
         for i in range(self.vectorSize):
